e3a25106404efb5affe326dfdfb789dfbacc0a68
abdularham2341 <abdulrhman3334@gmail.com>
2026-04-15 14:50:18 +0300

    Update main.h: Modify print_int to use buffer

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ int _write_buffer(char c);
 
 int print_char(va_list types);
 
-int print_int(int n);
+int print_int(int n, char buffer[], int *buff_ind);
 
 void print_buffer(char buffer[], int *buff_ind);
 
