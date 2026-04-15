61014af87cbc7e9f64e7607cb962988000a9fa44
abdularham2341 <abdulrhman3334@gmail.com>
2026-04-15 14:45:27 +0300

    Update print_int to use buffer

--- print_int.c
+++ print_int.c
@@ -3,10 +3,12 @@
 /**
  * print_int - prints an integer
  * @n: integer to print
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char
  *
  * Return: number of characters printed
  */
-int print_int(int n)
+int print_int(int n, char buffer[], int *buff_ind)
 {
 	unsigned int num;
 	int count = 0;
@@ -15,7 +17,9 @@ int print_int(int n)
 	if (n < 0)
 	{
 		c = '-';
-		write(1, &c, 1);
+		buffer[(*buff_ind)++] = c;
+		if (*buff_ind == BUFF_SIZE)
+			print_buffer(buffer, buff_ind);
 		count++;
 		num = (unsigned int)(-n);
 	}
@@ -25,10 +29,12 @@ int print_int(int n)
 	}
 
 	if (num / 10)
-		count += print_int(num / 10);
+		count += print_int(num / 10, buffer, buff_ind);
 
 	c = (num % 10) + '0';
-	write(1, &c, 1);
+	buffer[(*buff_ind)++] = c;
+	if (*buff_ind == BUFF_SIZE)
+		print_buffer(buffer, buff_ind);
 	count++;
 
 	return (count);
