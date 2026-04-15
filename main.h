1d0163a73b044355caf5d981c8292c1bebc6f3e9
abdularham2341 <abdulrhman3334@gmail.com>
2026-04-15 14:01:24 +0300

    Update main.h: add buff_size  macro and print_buffer prototype

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#define BUFF_SIZE 1024
 #include <stdarg.h>
 #include <unistd.h>
 
@@ -12,7 +12,7 @@ int print_char(va_list types);
 
 int print_int(int n);
 
-
+void print_buffer(char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
 
