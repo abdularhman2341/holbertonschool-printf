55a95c97d27358ab51363d2474255fb84bccc83a
abdularhman2341 <abdulrhman3334@gmail.com>
2026-04-17 23:24:21 +0000

    Handle precision

--- _printf.c
+++ _printf.c
@@ -13,6 +13,7 @@ int _printf(const char *format, ...)
 	int flag_plus, flag_space, flag_hash;
 	int len_long, len_short;
 	int width, out_len;
+	int precision, actual_digits, num_zeros, zero_case, str_len;
 	long int ln;
 	unsigned long int uln;
 	char *str;
@@ -65,6 +66,27 @@ int _printf(const char *format, ...)
 					i++;
 				}
 			}
+			precision = -1;
+			if (format[i] == '.')
+			{
+				i++;
+				precision = 0;
+				if (format[i] == '*')
+				{
+					precision = va_arg(args, int);
+					i++;
+				}
+				else
+				{
+					while (format[i] >= '0' && format[i] <= '9')
+					{
+						precision = precision * 10 + (format[i] - '0');
+						i++;
+					}
+				}
+				if (precision < 0)
+					precision = -1;
+			}
 			len_long = 0;
 			len_short = 0;
 			if (format[i] == 'l')
@@ -94,9 +116,12 @@ int _printf(const char *format, ...)
 					str = "(null)";
 				for (j = 0; str[j] != '\0'; j++)
 					;
-				if (width > j)
-					count += pad_spaces(width - j, buffer, &buff_ind);
-				for (j = 0; str[j] != '\0'; j++)
+				str_len = j;
+				if (precision >= 0 && precision < str_len)
+					str_len = precision;
+				if (width > str_len)
+					count += pad_spaces(width - str_len, buffer, &buff_ind);
+				for (j = 0; j < str_len; j++)
 				{
 					buffer[buff_ind++] = str[j];
 					if (buff_ind == BUFF_SIZE)
@@ -113,34 +138,49 @@ int _printf(const char *format, ...)
 				else
 					ln = va_arg(args, int);
 				if (ln < 0)
-					out_len = count_digits((unsigned long int)(-ln), 10) + 1;
-				else if (flag_plus || flag_space)
-					out_len = count_digits((unsigned long int)ln, 10) + 1;
+					uln = (unsigned long int)(-ln);
 				else
-					out_len = count_digits((unsigned long int)ln, 10);
+					uln = (unsigned long int)ln;
+				zero_case = (precision == 0 && uln == 0);
+				actual_digits = zero_case ? 0 : count_digits(uln, 10);
+				num_zeros = 0;
+				if (precision > actual_digits)
+					num_zeros = precision - actual_digits;
+				out_len = num_zeros + actual_digits;
+				if (ln < 0 || flag_plus || flag_space)
+					out_len++;
 				if (width > out_len)
 					count += pad_spaces(width - out_len, buffer, &buff_ind);
-				if (ln >= 0)
+				if (ln < 0)
 				{
-					if (flag_plus)
-					{
-						buffer[buff_ind++] = '+';
-						if (buff_ind == BUFF_SIZE)
-							print_buffer(buffer, &buff_ind);
-						count++;
-					}
-					else if (flag_space)
-					{
-						buffer[buff_ind++] = ' ';
-						if (buff_ind == BUFF_SIZE)
-							print_buffer(buffer, &buff_ind);
-						count++;
-					}
+					buffer[buff_ind++] = '-';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
 				}
-				if (len_long || len_short)
-					count += print_long(ln, buffer, &buff_ind);
-				else
-					count += print_int((int)ln, buffer, &buff_ind);
+				else if (flag_plus)
+				{
+					buffer[buff_ind++] = '+';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				else if (flag_space)
+				{
+					buffer[buff_ind++] = ' ';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				for (j = 0; j < num_zeros; j++)
+				{
+					buffer[buff_ind++] = '0';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				if (!zero_case)
+					count += print_ulong(uln, buffer, &buff_ind);
 			}
 			else if (format[i] == 'b')
 			{
@@ -154,13 +194,23 @@ int _printf(const char *format, ...)
 					uln = (unsigned short int)va_arg(args, unsigned int);
 				else
 					uln = va_arg(args, unsigned int);
-				out_len = count_digits(uln, 10);
+				zero_case = (precision == 0 && uln == 0);
+				actual_digits = zero_case ? 0 : count_digits(uln, 10);
+				num_zeros = 0;
+				if (precision > actual_digits)
+					num_zeros = precision - actual_digits;
+				out_len = num_zeros + actual_digits;
 				if (width > out_len)
 					count += pad_spaces(width - out_len, buffer, &buff_ind);
-				if (len_long || len_short)
+				for (j = 0; j < num_zeros; j++)
+				{
+					buffer[buff_ind++] = '0';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				if (!zero_case)
 					count += print_ulong(uln, buffer, &buff_ind);
-				else
-					count += print_unsigned((unsigned int)uln, buffer, &buff_ind);
 			}
 			else if (format[i] == 'o')
 			{
@@ -170,22 +220,33 @@ int _printf(const char *format, ...)
 					uln = (unsigned short int)va_arg(args, unsigned int);
 				else
 					uln = va_arg(args, unsigned int);
-				out_len = count_digits(uln, 8);
-				if (flag_hash && uln != 0)
-					out_len++;
+				zero_case = (precision == 0 && uln == 0);
+				actual_digits = zero_case ? 0 : count_digits(uln, 8);
+				num_zeros = 0;
+				if (precision > actual_digits)
+					num_zeros = precision - actual_digits;
+				if (flag_hash)
+				{
+					if (zero_case && uln == 0)
+					{
+						zero_case = 0;
+						num_zeros = 1;
+					}
+					else if (uln != 0 && num_zeros == 0)
+						num_zeros = 1;
+				}
+				out_len = num_zeros + actual_digits;
 				if (width > out_len)
 					count += pad_spaces(width - out_len, buffer, &buff_ind);
-				if (flag_hash && uln != 0)
+				for (j = 0; j < num_zeros; j++)
 				{
 					buffer[buff_ind++] = '0';
 					if (buff_ind == BUFF_SIZE)
 						print_buffer(buffer, &buff_ind);
 					count++;
 				}
-				if (len_long || len_short)
+				if (!zero_case && actual_digits > 0)
 					count += print_octal_long(uln, buffer, &buff_ind);
-				else
-					count += print_octal((unsigned int)uln, buffer, &buff_ind);
 			}
 			else if (format[i] == 'x')
 			{
@@ -195,7 +256,12 @@ int _printf(const char *format, ...)
 					uln = (unsigned short int)va_arg(args, unsigned int);
 				else
 					uln = va_arg(args, unsigned int);
-				out_len = count_digits(uln, 16);
+				zero_case = (precision == 0 && uln == 0);
+				actual_digits = zero_case ? 0 : count_digits(uln, 16);
+				num_zeros = 0;
+				if (precision > actual_digits)
+					num_zeros = precision - actual_digits;
+				out_len = num_zeros + actual_digits;
 				if (flag_hash && uln != 0)
 					out_len += 2;
 				if (width > out_len)
@@ -211,10 +277,15 @@ int _printf(const char *format, ...)
 						print_buffer(buffer, &buff_ind);
 					count++;
 				}
-				if (len_long || len_short)
+				for (j = 0; j < num_zeros; j++)
+				{
+					buffer[buff_ind++] = '0';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				if (!zero_case && actual_digits > 0)
 					count += print_hex_long(uln, buffer, &buff_ind);
-				else
-					count += print_hex((unsigned int)uln, buffer, &buff_ind);
 			}
 			else if (format[i] == 'X')
 			{
@@ -224,7 +295,12 @@ int _printf(const char *format, ...)
 					uln = (unsigned short int)va_arg(args, unsigned int);
 				else
 					uln = va_arg(args, unsigned int);
-				out_len = count_digits(uln, 16);
+				zero_case = (precision == 0 && uln == 0);
+				actual_digits = zero_case ? 0 : count_digits(uln, 16);
+				num_zeros = 0;
+				if (precision > actual_digits)
+					num_zeros = precision - actual_digits;
+				out_len = num_zeros + actual_digits;
 				if (flag_hash && uln != 0)
 					out_len += 2;
 				if (width > out_len)
@@ -240,10 +316,15 @@ int _printf(const char *format, ...)
 						print_buffer(buffer, &buff_ind);
 					count++;
 				}
-				if (len_long || len_short)
+				for (j = 0; j < num_zeros; j++)
+				{
+					buffer[buff_ind++] = '0';
+					if (buff_ind == BUFF_SIZE)
+						print_buffer(buffer, &buff_ind);
+					count++;
+				}
+				if (!zero_case && actual_digits > 0)
 					count += print_HEX_long(uln, buffer, &buff_ind);
-				else
-					count += print_HEX((unsigned int)uln, buffer, &buff_ind);
 			}
 			else if (format[i] == 'S')
 			{
